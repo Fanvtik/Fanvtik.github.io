eb13d202a3c5db31b56ebf4c2d71c32cf060f2d7
Zhena <erogovoj4@gmail.com>
2025-03-11 21:25:01 +0200

    init signific palces

--- memoryCards/script.js
+++ memoryCards/script.js
@@ -1,6 +1,9 @@
 'use strict'
 document.body.addEventListener('click',e=>{
     let el=e.target.closest('.grid-item');
+    if (e.target.closest('.carousel')) {
+        return;
+    }
     if(el){
        el.classList.toggle('clicked');
     }
